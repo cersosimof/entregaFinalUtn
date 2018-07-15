2d5ab3c167943a6262ee563e181c39c93279ebf7
cersosimof <cersosimo.facundo@gmail.com>
2018-07-15 16:29:03 -0300

    arreglo de imagen por default

--- myapp/routes/alta.js
+++ myapp/routes/alta.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var multer  = require('multer')
-// var upload = multer({ dest: './public/images/' })
-// -------------------------
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
       cb(null, './public/images/');
@@ -15,6 +13,12 @@ var storage = multer.diskStorage({
   
   var upload = multer({ storage: storage });
 //--------------------------------------------
+var pool  = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'proveedores'
+});
 
 
 router.get('/', function(req, res) {
@@ -25,30 +29,25 @@ router.get('/', function(req, res) {
     }
 })
 
-router.post('/', upload.single('imagen'), function(req, res, next) {         
+router.post('/', upload.single('imagen'), function(req, res, next) {
+
     var nombre = req.body.nombre;
     var correo = req.body.correo;
     var telefono = req.body.telefono;    
     var contacto = req.body.contacto;
     var ramo = req.body.ramo;
     var cuit = req.body.cuit;
-    var imagen = req.file.filename;
+    var imagen = 'noimagen.png'
+    
+    //si ingresa imagen reemplaza.
+    if(req.file) {
+    imagen = req.file.filename
+    };
 
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'proveedores'
-      });
-       
-      connection.connect();
-       
-      connection.query('INSERT INTO proveeores (nombre, correo, telefono, contacto, ramo, cuit, imagen) VALUES ( ?, ?, ?, ?, ?, ?, ?)', [nombre, correo, telefono, contacto, ramo, cuit, imagen], function (error, results, fields) {
-        if (error) throw error;
-        res.render('alta', {'altaOk' : 'Proveedor ingresado en la base de datos', 'usuario' : req.session.user })
-      });
-       
-      connection.end();
+    pool.query('INSERT INTO proveeores (nombre, correo, telefono, contacto, ramo, cuit, imagen) VALUES ( ?, ?, ?, ?, ?, ?, ?)', [nombre, correo, telefono, contacto, ramo, cuit, imagen], function (error, results, fields) {
+      if (error) throw error;
+      res.render('alta', {'altaOk' : 'Proveedor ingresado en la base de datos', 'usuario' : req.session.user })
+    });
 })
 
 module.exports = router;
